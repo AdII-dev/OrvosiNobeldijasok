14e7cce6a1713cd988392b7a2a3d704ca880b154
Ny19ViziA <Ny19ViziA@203-15>
2023-10-09 09:49:25 +0200

    Add project files.

---
+++ OrvosiNobeldijasok/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrvosiNobeldijasok
+{
+    class Nobeldijas
+    {
+        public int Ev { get; set; }
+        public string Nev { get; set; }
+        public string SzuletesHalalozas { get; set; }
+        public string Orszagkod { get; set; }
+    
+        public Nobeldijas(string sor)
+        {
+            var m = sor.Split(';');
+            Ev = int.Parse(m[0]);
+            Nev = m[1];
+            SzuletesHalalozas = m[2];
+            Orszagkod = m[3];
+
+
+        }
+    
+    }
+}
---
+++ OrvosiNobeldijasok/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings;
+using System.Collections.Generic;
+
+namespace OrvosiNobeldijasok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var nobeldijasok = new List<Nobeldijas>();
+            var sr = new StreamReader(
+                path: @"../../../Src/orvosi_nobeldijak.txt",
+                encoding: System.Text.Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) nobeldijasok.Add(new Nobeldijas(sr.ReadLine()));
+            
+            Console.WriteLine($"A díjazottak száma: {nobeldijasok.Count}");
+            //for (int i = 0; i < length; i++)
+            //{
+
+            //}
+            Console.WriteLine($"fasz ");
+            Console.ReadKey();
+        }
+    }
+}
